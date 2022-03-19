ed111694cb08dae0a3779b850a60beb7cfbd530a
Burak Can <drcan94@gmail.com>
2022-03-20 00:12:35 +0300

    Update no-shared-state.js

--- no-shared-state.js
+++ no-shared-state.js
@@ -6,8 +6,8 @@ const obj2 = {val: 3}
 
 console.log("\n--- With 'shared state' ---\n")
 
-console.log("obj1 için input -->",obj1)
-console.log("obj2 için input -->",obj2)
+console.log("obj1 ilk hali -->",obj1)
+console.log("obj2 ilk hali -->",obj2)
 
 const f1 = (object) => object.val += 1 
 const f2 = (object) => object.val *= 2 
@@ -18,12 +18,18 @@ const result2 = f2(obj1)
 const result3 = f2(obj2)
 const result4 = f1(obj2)
 
-console.log("obj1 için output -->",obj1)
-console.log("obj2 için output -->",obj2)
-console.log("result1 (f1) için output -->",result1)
-console.log("result2 (f2) için output -->",result2)
-console.log("result3 (f2) için output -->",result3)
-console.log("result4 (f1) için output -->",result4)
+console.log("obj1 son hali -->",obj1)
+console.log("obj2 son hali -->",obj2)
+console.log("f1 - obj1 için output -->",result1)
+console.log("f1 - obj2 için output -->",result4)
+console.log("f2 - obj1 için output -->",result2)
+console.log("f2 - obj2 için output -->",result3)
+console.log("f1(f2) - obj1 için output -->",f1({val:f2(obj1)}))
+console.log("f2(f1) - obj1 için output -->",f2({val:f1(obj1)}))
+console.log("f1(f2) - obj2 için output -->",f1({val:f2(obj2)}))
+console.log("f2(f1) - obj2 için output -->",f2({val:f1(obj2)}))
+console.log("obj1 en son hali -->",obj1)
+console.log("obj2 en son hali -->",obj2)
 // her iki obj de aslında aynı val değerini input olarak 
 // verirken outputlar değişiyor fonksiyonların çağrılma sıraları 
 // değiştiği için.. aynı zamanda fonksiyonlar her seferinde 
@@ -35,8 +41,8 @@ const obj3 = {val: 3}
 
 const obj4 = {val: 3}
 
-console.log("obj3 için input -->",obj3)
-console.log("obj4 için input -->",obj4)
+console.log("obj3 ilk hali -->",obj3)
+console.log("obj4 ilk hali -->",obj4)
 
 const f3 = (object) => {
     const newObj = {...object}
@@ -60,16 +66,18 @@ const result11 = f3({val:f4(obj4)})
 const result12 = f4({val:f3(obj4)})
 
 
-console.log("obj3 için output -->",obj3)
-console.log("obj4 için output -->",obj4)
-console.log("result5 (f3) için output -->",result5)
-console.log("result6 (f4) için output -->",result6)
-console.log("result7 (f4) için output -->",result7)
-console.log("result8 (f3) için output -->",result8)
-console.log("result9 (f3(f4)) - obj3 için output -->",result9)
-console.log("result10 (f4(f3)) - obj3 için output -->",result10)
-console.log("result11 (f3(f4)) - obj4 için output -->",result10)
-console.log("result12 (f4(f3)) - obj4 için output -->",result10)
+console.log("obj3 son hali -->",obj3)
+console.log("obj4 son hali -->",obj4)
+console.log("f3 - obj3 için output -->",result5)
+console.log("f4 - obj3 için output -->",result6)
+console.log("f3 - obj4 için output -->",result8)
+console.log("f4 - obj4 için output -->",result7)
+console.log("f3(f4) - obj3 için output -->",result9)
+console.log("f4(f3) - obj3 için output -->",result10)
+console.log("f3(f4) - obj4 için output -->",result11)
+console.log("f4(f3) - obj4 için output -->",result12)
+console.log("obj3 en son hali -->",obj3)
+console.log("obj4 en son hali -->",obj4)
 // obj'ler hiç değişmedi. sadece fonksiyon yeni bir obj döndürür ama 
 // onu bir yere eşitlersek sonucu görebiliriz.. fonksiyonlar her 
-// zaman aynı değeri veriyor
+// zaman aynı inputa aynı çıktıyı veriyor
